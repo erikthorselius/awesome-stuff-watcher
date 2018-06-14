1c60a803793d551db2204e0b538541a4fbee6140
Erik Thorselius <erik.thorselius@gmail.com>
2018-06-14 22:08:43 +0200

    Working selector

--- src/cljs/awesome_stuff_watcher/add_watcher.cljs
+++ src/cljs/awesome_stuff_watcher/add_watcher.cljs
@@ -1,64 +1,34 @@
 (ns awesome-stuff-watcher.add-watcher
   (:require [json-html.core :refer [edn->hiccup]]
-            [reagent.core :as reagent :refer [atom]]
-            [reagent-forms.core :refer [bind-fields init-field value-of]]))
-(defn row [label input]
-  [:div.row
-   [:div.col-md-2 [:label label]]
-   [:div.col-md-5 input]])
-
-(defn input [label type id]
-  (row label [:input.form-control {:field type :id id}]))
-
-(defn friend-source [text]
-  (filter
-    #(-> % (.toLowerCase %) (.indexOf text) (> -1))
-    ["Alice" "Alan" "Bob" "Beth" "Jim" "Jane" "Kim" "Rob" "Zoe"]))
-(def blocket [:div "Blocket"])
-(def systembolaget [:div "Systembolaget"])
-(defn extra-fields [watcher] (get {:blocket blocket
-                                   :systembolaget systembolaget} watcher))
-(def form-template
-  [:div
-   [:div.form-group
-    [:label "Bevaking"]
-    [:select.form-control {:field :list :id :watcher}
-     [:option {:key :blocket} "Blocket"]
-     [:option {:key :happyride} "Happy Ride"]
-     [:option {:key :systembolaget} "Systembolaget"]]]
-   (input "Sök text" :text :watcher.q)
-   [:div.row
-    [:div.col-md-2]
-    [:div.col-md-5
-     [:div.alert.alert-danger
-      {:field :alert :id :errors.q}]]]
-   (input "Notifiering" :text :watcher.notification)])
-
+            [reagent.core :as reagent :refer [atom]]))
+(defn watcher-selector [doc]
+  [:p
+   [:label "Bevaking"]
+   [:select.form-control {:placeholder "text" :on-change #(swap! doc assoc :watcher (.. % -target -value))}
+    [:option " "]
+    [:option {:value :blocket} "Blocket"]
+    [:option {:value :happyride} "Happy Ride"]
+    [:option {:value :systembolaget} "Systembolaget"]]])
+(defn blocket [] [:h1 "Blocket"])
+(defn happyride [] [:h1 "Happy ride"])
+(defn systembolaget [] [:h1 "Systembolaget"])
+(defn extra-fields [watcher] (case watcher
+                               "blocket" [blocket]
+                               "happyride" [happyride]
+                               "systembolaget" [systembolaget]
+                               [:p]))
+(defn search [doc] [:input {:type        "text"
+                            :placeholder "Sök..."
+                            :on-change   #(swap! doc assoc :q (-> % .-target .-value))}])
 (defn page []
   (let [doc (atom {})]
     (fn []
       [:div
        [:div.page-header [:h1 "Lägg till bevakare"]]
-
-       [bind-fields
-        form-template
-        doc
-        (fn [[id] value {:keys [weight-lb weight-kg] :as document}]
-          (cond
-            (= id :weight-lb)
-            (assoc document :weight-kg (/ value 2.2046))
-            (= id :weight-kg)
-            (assoc document :weight-lb (* value 2.2046))
-            :else nil))
-        (fn [[id] value {:keys [height weight] :as document}]
-          (when (and (some #{id} [:height :weight]) weight height)
-            (assoc document :bmi (/ weight (* height height)))))]
-
-       [:button.btn.btn-default
-        {:on-click
-         #(if (empty? (get-in @doc [:person :first-name]))
-            (swap! doc assoc-in [:errors :first-name]"first name is empty"))}
-        "save"]
+       [watcher-selector doc]
+       [search doc]
+       [extra-fields (:watcher @doc)]
        [:hr]
        [:h1 "Document State"]
-       [edn->hiccup @doc]])))
+       [edn->hiccup @doc]]
+      )))
